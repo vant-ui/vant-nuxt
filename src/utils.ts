a2e94732db915485d0753df6b151460e8b47e4c2
qiang <qw13131wang@gmail.com>
2025-05-28 20:51:13 +0800

    feat: support for use in the nuxt layers (#47)

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,19 @@
+import type { NuxtConfigLayer } from '@nuxt/schema'
+import { libraryName } from './config'
+
+export function getLayersDir (layers: NuxtConfigLayer[]) {
+  const list = []
+
+  for (const layer of layers) {
+    const srcDir = layer.config.srcDir || layer.cwd
+    if (srcDir.includes('node_modules') && layer.config[libraryName]?.importStyle !== false) {
+      list.push(srcDir)
+    }
+  }
+
+  return list
+}
+
 export function isObject (value: unknown): value is Record<any, any> {
   return value !== null && typeof value === 'object'
 }
@@ -5,7 +21,7 @@ export function isObject (value: unknown): value is Record<any, any> {
 export function toArray<T extends any | any[]> (
   value: T
 ): T extends any[] ? T : T[] {
-  return Array.isArray(value) ? value : ([value] as any)
+  return (Array.isArray(value) ? value : [value]) as any
 }
 
 export function toRegExp (arr: string[], flags?: string): RegExp {
